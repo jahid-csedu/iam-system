3bb6f4a4acaad83e69e888d8658da50c3f535ec5
Md. Jahid Hasan <jahid.csedu@gmail.com>
2024-11-06 08:50:22 +0900

    Add permission check method to UserService
    Introduce `hasPermission` method to UserService to streamline permission checks. Refactor AuthorizationAspect to utilize this new method, improving the readability and maintainability of the permission verification process.

--- src/main/java/com/example/iamsystem/aspect/AuthorizationAspect.java
+++ src/main/java/com/example/iamsystem/aspect/AuthorizationAspect.java
@@ -1,6 +1,7 @@
 package com.example.iamsystem.aspect;
 
 import com.example.iamsystem.exception.NoAccessException;
+import com.example.iamsystem.permission.PermissionAction;
 import com.example.iamsystem.user.UserService;
 import com.example.iamsystem.util.authorization.RequirePermission;
 import lombok.RequiredArgsConstructor;
@@ -11,26 +12,18 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.atomic.AtomicReference;
-
 @Aspect
 @Component
 @RequiredArgsConstructor
 public class AuthorizationAspect {
 
-    private final UserService authorizationService;
+    private final UserService userService;
 
     @Before("@annotation(requirePermission)")
     public void checkPermission(JoinPoint joinPoint, RequirePermission requirePermission) {
-        AtomicReference<Boolean> access = new AtomicReference<>(false);
         UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        userDetails.getAuthorities().forEach(authority -> {
-            if (authority.getAuthority().equals(requirePermission.serviceName() + ":" + requirePermission.action())) {
-                access.set(true);
-            }
-        });
 
-        if (Boolean.FALSE.equals(access.get())) {
+        if (!userService.hasPermission(userDetails, requirePermission.serviceName(), requirePermission.action())) {
             throw new NoAccessException("User does not have permission to perform this action");
         }
     }
